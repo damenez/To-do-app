6b9df981a9d24da4b5c274e5159553d6b1bea974
Dennis <dennis.m.basco@gmail.com>
2018-06-19 06:44:41 +0000

    User scope Todo version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,30 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success
 
+# dmb
+  # def authenticate_user!
+  #   if current_user.nil?
+  #     # redirect_to '/admin/login', alert: "You should login first to visit that page."
+  #     redirect_to '/login', alert: "You should login first to visit that page."
+  #   end
+  # end
+  # dmb
+
+
   private
   def logged_in?
     current_user
+ # dmb  current_user != nil
   end
   helper_method :logged_in?
 
+  # dmb commented out to try below routine
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+ 
   helper_method :current_user
-
+  # byebug
   def require_user
     if current_user
       true
